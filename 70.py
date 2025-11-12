a5de95efc3b81b4793d411bc17c80170caef8a86
Gabriele <dotigabriele@gmail.com>
2025-11-12 20:53:30 +0100

    some others...

--- 70.py
+++ 70.py
@@ -4,19 +4,19 @@ class Solution:
         # per arrivare a n posso:
         # 1) togliere 1 a n
         # 2) togliere 2 a n
-        # f restituisce un itero: numero di volte
+        # f restituisce un intero: numero di modi
 
         def f(n, d = {}) -> int:
             if n == -1:
                 return 0 # case 1-2=-1
             if n == 0:
                 return 1
-            # if the result is in the dictonary
+            # if the result is already in the dictionary
             if n in d:
                 return d[n]
-            # register the values in the dictonary
-            d[n-1] = f(n-1, d)
-            d[n-2] = f(n-2, d)
+            # compute and register the value
+            d[n] = f(n-1, d) + f(n-2, d)
             # give the result
-            return f(n-1, d) + f(n-2, d)
+            return d[n]
+
         return f(n)
